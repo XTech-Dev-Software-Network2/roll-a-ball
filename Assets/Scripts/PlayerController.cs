be1005028f8f634afc3e57a36832e6d36e056429
Junpei Higashi <37029324+jnphgs@users.noreply.github.com>
2020-10-05 00:48:15 +0900

    WIP lecture2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public UnityEngine.UI.Text DebugText;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        var acc = Input.gyro.rotationRate;
+        var f = new Vector3(acc.y, 0.0f, -acc.x);
+        _rigidbody.AddForce(20.0f*f);
+        DebugText.text = $"{acc.x}\n" +
+                         $"{acc.y}\n" +
+                         $"{acc.z}";
+    }
+}
