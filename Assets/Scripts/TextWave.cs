4649874f84e09dad42ffa62a3f89dc457e0d0c6a
Junpei Higashi <37029324+jnphgs@users.noreply.github.com>
2020-10-04 18:57:09 +0900

    lecture01

---
+++ Assets/Scripts/TextWave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TextWave : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+    [SerializeField] private AnimationCurve animationCurve; 
+
+    private void Awake()
+    {
+        _rectTransform = this.gameObject.GetComponent<RectTransform>();
+    }
+    
+    private void Update()
+    {
+        var rectPos = _rectTransform.anchoredPosition;
+        rectPos.y = animationCurve.Evaluate(Time.time);
+        _rectTransform.anchoredPosition = rectPos;
+    }
+}
