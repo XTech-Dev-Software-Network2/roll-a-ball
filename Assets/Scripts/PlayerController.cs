ec837d161186ae516bb3afcb6d006c897542019b
Junpei Higashi <mails@jnphgs.net>
2020-10-14 18:21:01 +0900

    Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,9 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        Input.gyro.enabled = true;
     }
 
     void Update()
     {
-        var acc = Input.gyro.rotationRate;
-        var f = new Vector3(acc.y, 0.0f, -acc.x);
-        _rigidbody.AddForce(20.0f*f);
-        DebugText.text = $"{acc.x}\n" +
-                         $"{acc.y}\n" +
-                         $"{acc.z}";
     }
 }
