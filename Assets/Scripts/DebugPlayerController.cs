e5824a4c095c8dbfe20ca3d18d0bf220611deec4
Junpei Higashi <mails@jnphgs.net>
2020-10-19 01:13:47 +0900

    WIP

---
+++ Assets/Scripts/DebugPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DebugPlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public UnityEngine.UI.Text DebugText;
+    
+    private void Awake() // 最初に1回呼ばれる
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
+    }
+
+    void Update() // 毎フレーム呼ばれる
+    {
+        var rotRate = Input.gyro.rotationRate;
+        DebugText.text = rotRate.ToString();
+        var force = new Vector3(rotRate.y, 0.0f, -rotRate.x);
+        _rigidbody.AddForce(20*force);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,16 @@
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    public UnityEngine.UI.Text DebugText;
     
-    private void Awake() // 最初に1回呼ばれる
+    private void Awake() 
     {
         _rigidbody = GetComponent<Rigidbody>();
-        Input.gyro.enabled = true;
     }
 
     void Update() // 毎フレーム呼ばれる
     {
-        var rotRate = Input.gyro.rotationRate;
-        DebugText.text = rotRate.ToString();
-        var force = new Vector3(rotRate.y, 0.0f, -rotRate.x);
-        _rigidbody.AddForce(20*force);
+        
+        var force = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        _rigidbody.AddForce(5*force);
     }
 }
