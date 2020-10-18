644051971d4fb889b01296689eccb8e160c6480a
Junpei Higashi <mails@jnphgs.net>
2020-10-18 21:21:58 +0900

    ジャイロセンサーの値を利用する。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     public UnityEngine.UI.Text DebugText;
-
-    // Update is called once per frame
-    private void Awake()
+    
+    private void Awake() // 最初に1回呼ばれる
     {
         _rigidbody = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
     }
 
-    void Update()
+    void Update() // 毎フレーム呼ばれる
     {
+        var rotRate = Input.gyro.rotationRate;
+        DebugText.text = rotRate.ToString();
+        var force = new Vector3(rotRate.y, 0.0f, -rotRate.x);
+        _rigidbody.AddForce(20*force);
     }
 }
