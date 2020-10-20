8bc91a95821c6396a48cb65ad84631e3fd4028d5
Junpei Higashi <mails@jnphgs.net>
2020-10-21 02:34:21 +0900

    シーンの更新

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    public List<GameObject> ActiveWhenFinish = new List<GameObject>();
     
     private void Awake() 
     {
         _rigidbody = GetComponent<Rigidbody>();
+        foreach (var obj in ActiveWhenFinish)
+        {
+            obj.SetActive(false);
+        }
     }
 
     void Update() // 毎フレーム呼ばれる
@@ -16,4 +22,17 @@ public class PlayerController : MonoBehaviour
         var force = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         _rigidbody.AddForce(5*force);
     }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Finish");
+            foreach (var obj in ActiveWhenFinish)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
 }
